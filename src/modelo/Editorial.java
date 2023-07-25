4cf502c1c397cee4cff0bfbf3500c511404fadd5
IFCD0111ADRI <IFCD0111ADRI@DESKTOP-74Q25UV>
2023-07-25 14:17:35 +0200

    inicial editoriales

---
+++ src/modelo/Editorial.java
@@ -0,0 +1,66 @@
+package modelo;
+
+import java.util.Objects;
+
+import excepciones.CantidadDebeSerPositivaException;
+
+public class Editorial {
+	private String nombre;
+	private int codEditorial;
+	private int anio;
+
+	public Editorial(String nombre, int codEditorial, int anio) {
+		super();
+		this.codEditorial = codEditorial;
+		// llamamos al setter para ejercer el control de valores negativos
+		this.setCodEditorial(codEditorial);
+		this.setAnio(anio);
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCodEditorial() {
+		return codEditorial;
+	}
+
+	public void setCodEditorial(int codEditorial) {
+		this.codEditorial = codEditorial;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	@Override
+	public String toString() {
+		return "Editorial [nombre=" + nombre + ", codEditorial=" + codEditorial + ", anio=" + anio + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codEditorial);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Editorial other = (Editorial) obj;
+		return codEditorial == other.codEditorial;
+	}
+}
